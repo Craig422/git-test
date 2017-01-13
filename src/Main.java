61885716676696c84bef27bd37abecca37a33599
Craig <craig@422.com>
2017-01-13 10:08:04 +0000

    Added Test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Git 2nd");
+
+
+        System.out.println("Git 6th");
+
+
     }
 }
---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Craig on 13/01/2017.
+ */
+public class Test1 {
+
+    void test(){
+        System.out.println("Test");
+    }
+}
